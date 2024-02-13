d0f927e001069dcd77a53eea59b31c7de9b264bc
yoonsoo_park <yoonsoo.park@ncino.com>
2024-02-13 18:59:44 -0500

    Revert "upd: get only certain pkg based on tag"
    This reverts commit 78415d4d363e961faeaa9bcad02f92a9d347565b.

--- src/commands/metaman/permission/list.ts
+++
@@ -1,9 +0,0 @@
-import {Command} from '@oclif/core';
-
-export class SubListCommand extends Command {
-  static description = 'list';
-
-  async run() {
-    this.log('Running Permission list... with Topics');
-  }
-}
